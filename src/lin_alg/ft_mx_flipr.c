33c692151b4917448f54af3d9821be788b78fa5d
Luis Luque <luque.luis@gmail.com>
2024-03-05 10:53:47 +0100

    Flip matrix function changed
    For convenience the ft_mx_flipr() function now changes the
    matrix passed as argument and returns void.
    Now ft_mx_load_file() function takes care of flipping the
    map matrix loaded from file instead of fdf_tesselate_map().

--- src/lin_alg/ft_mx_flipr.c
+++ src/lin_alg/ft_mx_flipr.c
@@ -6,21 +6,27 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 19:45:50 by lluque            #+#    #+#             */
-/*   Updated: 2024/02/27 20:49:13 by lluque           ###   ########.fr       */
+/*   Updated: 2024/03/05 00:56:51 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "lin_alg.h"
 
-t_ft_mx	*ft_mx_flipr(t_ft_mx *matrix)
+int	ft_mx_flipr(t_ft_mx *matrix)
 {
 	t_ft_mx	*r;
 	t_ft_mx	*bid;
 
 	bid = ft_mx_create_bid(matrix->n);
 	if (bid == NULL)
-		return (NULL);
+		return (0);
 	r = ft_mx_mult(matrix, bid);
 	ft_mx_destroy(bid);
-	return (r);
+	if (r == NULL)
+		return (0);
+	free(matrix->d);
+	matrix->d = r->d;
+	free(r);
+	return (1);
 }
--- src/tesselator/fdf_tesselate_map.c
+++ src/tesselator/fdf_tesselate_map.c
@@ -6,7 +6,7 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:05:33 by lluque            #+#    #+#             */
-/*   Updated: 2024/03/01 23:17:10 by lluque           ###   ########.fr       */
+/*   Updated: 2024/03/04 13:09:36 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,15 @@
 t_fdf_model	*fdf_tesselate_map(t_ft_mx	*map_mx)
 {
 	t_fdf_model	*model;
-	t_ft_mx		*transformed_map;
-
-	ft_printf("\t[fdf_tesselate_map] Flipping map matrix...'\n");
-	transformed_map = ft_mx_flipr(map_mx);
 	model = ft_calloc(1, sizeof (t_fdf_model));
 	if (model == NULL)
-		return (ft_mx_destroy(transformed_map), NULL);
+		return ( NULL);
 	ft_printf("\t[fdf_tesselate_map] Getting vertex matrix from map...'\n");
-	model->vertex_mx = fdf_get_vertex_mx(transformed_map);
+	model->vertex_mx = fdf_get_vertex_mx(map_mx);
 	if (model->vertex_mx == NULL)
 	{
 		ft_printf("There was a problem while getting vertex_mx\n");
-		return (ft_mx_destroy(transformed_map), NULL);
+		return (NULL);
 	}
-	return (ft_mx_destroy(transformed_map), model);
+	return (model);
 }
