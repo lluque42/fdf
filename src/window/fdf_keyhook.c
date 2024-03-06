2b89205fce04d9eddd64325221da9ad270730b91
Luis Luque <luque.luis@gmail.com>
2024-03-06 02:05:17 +0100

    Now re-rendering on key events to rotate!!!
    For now, there is too much code on hook functions. The next step
    will be to create high-level GUI event management functions and
    leave only at hook level the detection of each event and the call
    to the specific management function that reacts to each event.

--- src/window/fdf_keyhook.c
+++ src/window/fdf_keyhook.c
@@ -6,14 +6,14 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:06:57 by lluque            #+#    #+#             */
-/*   Updated: 2024/03/05 14:11:18 by lluque           ###   ########.fr       */
+/*   Updated: 2024/03/06 02:01:34 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "window.h"
+#include "drawing.h"
 #include "libft.h"
 
-// Supone que en param viene un mlx_t. Esto se logra al registrar este callback
 void	fdf_keyhook(mlx_key_data_t keydata, void *f)
 {
 	t_fdf	*fdf;
@@ -23,5 +23,41 @@ void	fdf_keyhook(mlx_key_data_t keydata, void *f)
 	{
 		ft_printf("Escape (%p)\n", fdf);
 		mlx_close_window(fdf->wlayout->window);
+	}
+	if (keydata.key == MLX_KEY_X && keydata.action == MLX_RELEASE)
+	{
+		fdf->c_deg_x += 5;
+		if (fdf->c_deg_x > 360)
+			fdf->c_deg_x -= 360;
+		ft_printf("New X rotation angle: %d\n", (int)fdf->c_deg_x);
+		if (!fdf_render(fdf, FROM_CAMERA))
+		{
+			ft_printf("Error while rendering from rotate-X event\n");
+			mlx_close_window(fdf->wlayout->window);
+		}
+	}
+	if (keydata.key == MLX_KEY_Y && keydata.action == MLX_RELEASE)
+	{
+		fdf->c_deg_y += 5;
+		if (fdf->c_deg_y > 360)
+			fdf->c_deg_y -= 360;
+		ft_printf("New Y rotation angle: %d\n", (int)fdf->c_deg_y);
+		if (!fdf_render(fdf, FROM_CAMERA))
+		{
+			ft_printf("Error while rendering from rotate-X event\n");
+			mlx_close_window(fdf->wlayout->window);
+		}
+	}
+	if (keydata.key == MLX_KEY_Z && keydata.action == MLX_RELEASE)
+	{
+		fdf->c_deg_z += 5;
+		if (fdf->c_deg_z > 360)
+			fdf->c_deg_z -= 360;
+		ft_printf("New Z rotation angle: %d\n", (int)fdf->c_deg_z);
+		if (!fdf_render(fdf, FROM_CAMERA))
+		{
+			ft_printf("Error while rendering from rotate-X event\n");
+			mlx_close_window(fdf->wlayout->window);
+		}
 	}
 }
