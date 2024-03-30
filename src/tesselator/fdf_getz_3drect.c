df965ada19f4fa71c8047d9f595cde3ec4e1cef3
Luis Luque <luque.luis@gmail.com>
2024-03-30 12:20:02 +0100

    FIX 3D rect functions
    Added a fix to fdf_getz_3drect.c functions: the special
    cases had either incomplete conditions or wrong return
    values. Also the legibility was improved. This program
    doesn't required such detail level, that's is why this
    mistake had not been detected.

--- src/tesselator/fdf_getz_3drect.c
+++ src/tesselator/fdf_getz_3drect.c
@@ -6,7 +6,7 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 20:06:41 by lluque            #+#    #+#             */
-/*   Updated: 2024/03/21 11:10:48 by lluque           ###   ########.fr       */
+/*   Updated: 2024/03/27 15:31:20 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ int	fdf_getz_3drect_fromx(double *z, t_fdf_3drect *rect, double x)
 	double	tx;
 	double	tz;
 
-	if (rect->parallel_to_yz)
+	if (rect->parallel_to_yz && rect->parallel_to_xz)
+		return (0);
+	if (rect->parallel_to_yz && rect->parallel_to_yz_at_x != x)
 		return (0);
 	if (rect->parallel_to_xy)
 	{
 		*z = rect->parallel_to_xy_at_z;
-		return (0);
+		return (1);
 	}
 	tx = rect->x2 - rect->x1;
 	tz = rect->z2 - rect->z1;
@@ -37,12 +39,14 @@ int	fdf_getz_3drect_fromy(double *z, t_fdf_3drect *rect, double y)
 	double	ty;
 	double	tz;
 
-	if (rect->parallel_to_xz)
+	if (rect->parallel_to_yz && rect->parallel_to_xz)
+		return (0);
+	if (rect->parallel_to_xz && rect->parallel_to_xz_at_y != y)
 		return (0);
 	if (rect->parallel_to_xy)
 	{
 		*z = rect->parallel_to_xy_at_z;
-		return (0);
+		return (1);
 	}
 	ty = rect->y2 - rect->y1;
 	tz = rect->z2 - rect->z1;
