ea17966f11ce4b350b0b7538bec1fb06a2a219a6
Luis Luque <luque.luis@gmail.com>
2024-07-21 14:38:20 +0200

    WIP ft_mx_load_file.c, color matrix loaded
    The work on ft_mx_load_file.c continues while maintaining
    compatibility with the caller of ft_mx_load_file().
    The matrix representing the map color value for each is succesfuly
    filled with the color values.
    The next steps will be to modify the ft_mx_load_file() caller
    (the main()) and downstream from there.

--- src/mandatory/lin_alg/ft_mx_load_file.c
+++ src/mandatory/lin_alg/ft_mx_load_file.c
@@ -6,7 +6,7 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:00:31 by lluque            #+#    #+#             */
-/*   Updated: 2024/07/21 13:47:14 by lluque           ###   ########.fr       */
+/*   Updated: 2024/07/21 14:30:34 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 #include <stdint.h>														// for uint32_t
 #include "libft.h"
 #include "lin_alg.h"
+
+#define Z 0
+#define C 1
+
+
+
 
 // Takes a line with words separated by a DELIMITER, creates a NULL terminated
 // array with said substrings and returns the number of substrings or 0 if
@@ -77,48 +83,60 @@ static t_ft_mx	*create_matrix(char *filename,
 	return (close(fd), matrix);
 }
 
-static int	parse_line(char *line, char separator, int row, t_ft_mx *matrix)
+static int	parse_line(char *line, char separator, int row, t_ft_mx **mx)
 {
 	int		col;
 	char	**col_str_arr;
 	char	*color_comma_pos;
 	char	*trimmed_line;
+	int		color;
 
 	trimmed_line = ft_strtrim(line, " \n\t");
 	line = trimmed_line;
 	col_str_arr = ft_split(line, separator);
 	if (col_str_arr == NULL)
 		return (0);
-	if (get_number_of_cols(line, separator) != matrix->n)
+	if (get_number_of_cols(line, separator) != mx[Z]->n)
 		return (free(trimmed_line), ft_free_strarr_from(col_str_arr, 0), 0);
 	col = -1;
-	while (++col < matrix->n)
+	while (++col < mx[Z]->n)												////	This also assumes.... 
 	{
 		color_comma_pos = ft_strchr(col_str_arr[col], ',');
 		if (color_comma_pos != NULL)
 		{
 
 
-			/*
 
-			int	is_error;
-			int	color;
+			//int	is_error;
 
-			is_error = 0;
-			ft_printf("Color info appears to be: '%s'\n", color_comma_pos);
+			//is_error = 0;
+			//ft_printf("Color info appears to be: '%s'\n", color_comma_pos);
 			// checking basic prefix validity
 			if (ft_strncmp(color_comma_pos, ",0x", 3) != 0)
-				ft_printf("Color info MALFORMED!!!\n");
-				is_error = 1;
+				return (free(trimmed_line),
+						ft_free_strarr_from(col_str_arr, 0), 0);
+			//{
+			//	ft_printf("Color info MALFORMED!!!\n");
+			//	is_error = 1;
+			//}
 			color = ft_atoi_b(color_comma_pos + 3, 16);
 			if (color == -1)
-				is_error = 1;
+				return (free(trimmed_line),
+						ft_free_strarr_from(col_str_arr, 0), 0);
+				//is_error = 1;
+			mx[C]->d[row * mx[C]->n + col] = color;
+
+
+
+/*
 			if (!is_error)
 				ft_printf("Color value: '%d'\n", (uint32_t)color);
 			else
+			{
+				// return 0 after freeing everything
 				ft_printf("Color info is malformed!!!!\n");
-			*/
-
+			}
+*/
 
 
 
@@ -127,15 +145,12 @@ static int	parse_line(char *line, char separator, int row, t_ft_mx *matrix)
 		}
 		if (!ft_aisi(col_str_arr[col]))
 			return (free(trimmed_line), ft_free_strarr_from(col_str_arr, 0), 0);
-		matrix->d[row * matrix->n + col] = (double)ft_atoi(col_str_arr[col]);
+		mx[Z]->d[row * mx[Z]->n + col] = (double)ft_atoi(col_str_arr[col]);
 	}
 	return (free(trimmed_line), ft_free_strarr_from(col_str_arr, 0), 1);
 }
 
 
-
-#define Z 0
-#define C 1
 
 // Array of pointers to matrix, where index Z (0) refers to the matrix with
 // the Z values and index C (1) to the matric with the color values.
@@ -148,7 +163,6 @@ t_ft_mx	*ft_mx_load_file(char *filename, char separator)
 
 	map_mx[Z] = create_matrix(filename, separator, 0);
 	map_mx[C] = create_matrix(filename, separator, 255);
-	ft_mx_print(map_mx[C], ' ');
 	if (map_mx[Z] == NULL || map_mx[C] == NULL)
 		return (ft_mx_destroy(map_mx[Z]), ft_mx_destroy(map_mx[C]), NULL);
 	fd = open(filename, O_RDONLY);
@@ -164,13 +178,14 @@ t_ft_mx	*ft_mx_load_file(char *filename, char separator)
 	//		return (ft_mx_destroy(matrix), close(fd), NULL);
 		
 		// FOR NOW, only pass Z values matrix to check that everything is OK
-		if (!parse_line(line, separator, row, map_mx[Z]))
+		if (!parse_line(line, separator, row, map_mx))
 	//		return (free(line), ft_free_gnl(fd),
 	//			ft_mx_destroy(matrix), close(fd), NULL);
 			return (free(line), ft_free_gnl(fd), ft_mx_destroy(map_mx[Z]),
 					ft_mx_destroy(map_mx[C]), close(fd), NULL);
 		free(line);
 	}
+	ft_mx_print(map_mx[C], ' ');
 	//FOR NOW only return the Z values map matrix, for compatibility with the
 	//caller.
 	return (ft_free_gnl(fd), close(fd), map_mx[Z]);
